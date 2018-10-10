b43b4c44fe1f87cf09af318941bdc318b0653ab9
Tom Chambers <tom.chambers@gmail.com>
2018-10-10 16:41:52 +0100

    add config

--- packages/jest-config/jest-preset.js
+++ packages/jest-config/jest-preset.js
@@ -1,12 +1,9 @@
 module.exports = {
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/**/*.js'],
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.js"],
   collectCoverage: true,
-  testPathIgnorePatterns: ['/dist/', '/node_modules/(?!@times-tooling)'],
+  testPathIgnorePatterns: ["/dist/", "/node_modules/(?!@times-tooling)"],
   moduleNameMapper: {
-    '\\.(scss|css)$': 'identity-obj-proxy',
-  },
-  setupFiles: ['../../__test__/setupTests.js'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  resolver: 'jest-workspace-resolver',
+    "\\.(scss|css)$": "identity-obj-proxy"
+  }
 };
